9ae10d6631d7440f31d9b29fa0fe028ed9c62ee4
O L A <114856145+olaReposit@users.noreply.github.com>
2022-10-02 12:33:54 +0100

    v1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-const mobileBtn = document.getElementById('mobile-cta')
-const nav = document.querySelector('nav')
-const mobileBtnExit = document.getElementById('mobile-exit-cta');
+let mobileBtn = document.getElementById('mobile-cta')
+let nav = document.querySelector('nav')
+let mobileBtnExit = document.getElementById('mobile-exit-cta');
 
 mobileBtn.addEventListener('click', ()=>{
   nav.classList.add('menu-btn');
